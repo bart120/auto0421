fb67b1f0da89dd4108110721be9498fda23d6273
Vincent <vleclerc@inow.fr>
2021-04-21 15:30:54 +0200

    guards

--- src/app/car/car-routing.module.ts
+++ src/app/car/car-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListComponent } from './list/list.component';
 import { AddComponent } from './add/add.component';
 import { DetailComponent } from './detail/detail.component';
+import { AuthenticationGuard } from '../guards/authentication.guard';
 
 const childRoutes: Routes = [
     { path: 'list', component: ListComponent },
-    { path: 'add', component: AddComponent },
+    { path: 'add', component: AddComponent, canActivate: [AuthenticationGuard] },
     { path: 'detail/:id/:name', component: DetailComponent }
 ]
 
